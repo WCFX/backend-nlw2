6874fcd99c8d5bb945ba78720b2b59d4364dd1f2
wcfx <wcfx.dev@gmail.com>
2020-11-10 21:03:35 -0300

    Rota para criacao de aula feito. Listagem de aulas

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json())
-
-
-app.get('/', (req, res) => {
-  return res.json({ message: 'Caso queira ver a lista de usuários, vá para a página /users'})
-})
-
-
-app.post('/users', (req, res) => {
-
-  console.log(req.body)
-
-  const users = [
-    {name: 'Wagner', idade: 26},
-    {name: 'Cintia', idade: 29},
-    {name: 'Antonella', idade: 2}
-
-  ]
-  return res.json(users);
-
-});
+app.use(routes);
 
 app.listen(3333);
